54c4ab293d17a5339f48a4252b22e1cb7dd0cc78
Roberto Carrasco <titos.carrasco@gmail.com>
2014-03-30 21:12:19 -0300

    Added a GUI using GLADE and GTK3

--- Test_Joystick.py
+++ Test_Joystick.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from gi.repository import Gtk
+from threading import Thread
 import pygame
 import time
 
@@ -8,80 +10,148 @@ import sys
 sys.path.append('./')
 from DaguCar import DaguCar
 
-def AddText(text, left, top):
-	font = pygame.font.Font(None, 20)
-	text = font.render(text, 1, (0, 0, 0))
-	textpos = text.get_rect()
-	textpos.left = left
-	textpos.top  = top
-	return text, textpos
+class Main:
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("Resources/DaguCar.glade")
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("MainWindow")
+        self.window.show_all()
+        self.dc = None
+        self.MoveButtons = self.builder.get_object("MoveButtons")
+        self.Speed = self.builder.get_object("Speed")
+        self.Speed.props.adjustment=self.builder.get_object("SpeedAdjustment")
+        self.Port = self.builder.get_object("Port")
+        self.Connect = self.builder.get_object("Connect")
+        self.StatusBar = self.builder.get_object("StatusBar")
+        self.SbContextId = self.StatusBar.get_context_id("MainMessages")
+        self.TJoystick = None
 
-def Main():
-	pygame.init()
-	if(pygame.joystick.get_count()<=0):
-		print 'No Joystick detected: Aborting.'
-		pygame.quit()
-		return
 
-	joystick = pygame.joystick.Joystick(0)
-	joystick.init()
+    def OnDeleteWindow(self, *args):
+        self.OnQuit(*args)
 
-	buttons = [0]*joystick.get_numbuttons()
-	axes = [0]*joystick.get_numaxes()
 
-	screen = pygame.display.set_mode((320, 240))
-	pygame.display.set_caption('DaguCar Test')
-	background = pygame.Surface(screen.get_size())
-	background = background.convert()
-	background.fill((0xff, 0xff, 0xff))
-	background.blit(*AddText("Button 1: More speed", 10, 10))
-	background.blit(*AddText("Button 3: Less speed", 10, 25))
-	background.blit(*AddText("Button 5: Quit", 10, 40))
+    def OnQuit(self, *args):
+        self.Connect.set_active(False)
+        Gtk.main_quit()
+        
 
-	speedUp = 0
-	speedDown = 0
+    def OnActive(self, *args):
+        if(self.Connect.get_active()):
+            self._SbSetMessage()
+            self.dc = DaguCar(self.Port.get_text())
+            if(self.dc!=None and self.dc.IsConnected()):
+                self.MoveButtons.set_sensitive(True)
+                self.Speed.set_sensitive(True)
+                self.Port.set_sensitive(False)
+                self.dc.SetSpeed(int(self.Speed.props.adjustment.get_value()))
+                self._SbSetMessage("Conectado a %s" % (self.Port.get_text()))
+                pygame.init()
+                if(pygame.joystick.get_count()>0):
+                    self.TJoystick = Thread(target=self._Joystick, args=())
+                    self.TJoystick.start()
+            else:
+                self.Connect.set_active(False);
+                self.dc = None
+                self._SbSetMessage("Error al conectar")
+        else:
+            if(self.dc!=None):
+                self.dc.Close()
+                self.dc = None
+                self.MoveButtons.set_sensitive(False)
+                self.Speed.set_sensitive(False)
+                self.Port.set_sensitive(True)
+                self._SbSetMessage("Desconectado")
+                if(self.TJoystick!=None):
+                    self.TJoystick.join()
+                    self.TJoystick = None
+                pygame.quit()
+        return
+        
 
-	dc = DaguCar('/dev/rfcomm0')
-	if(not dc.IsConnected()):
-		pygame.quit()
-		return;
+    def OnSpeedChanged(self, *args):
+        self.dc.SetSpeed(int(self.Speed.props.adjustment.get_value()))
+        
 
-	while(True):
-		pygame.event.pump()
-		try:
-			for i in range(len(buttons)):
-				buttons[i] = joystick.get_button(i)
+    def OnUp(self, *args):
+        self.dc.Move(0, 1)
+            
 
-			if(buttons[4]==1):
-				break
-			elif(buttons[0]==1 and speedUp==0):
-				speedUp = 1
-			elif(buttons[0]==0 and speedUp==1):
-				speedUp = 0
-				dc.SetSpeed(dc.GetSpeed() + 1)
-			elif(buttons[2]==1 and speedDown==0):
-				speedDown = 1
-			elif(buttons[2]==0 and speedDown==1):
-				speedDown = 0
-				dc.SetSpeed(dc.GetSpeed() - 1)
+    def OnUpLeft(self, *args):
+        self.dc.Move(-1, 1)
+            
 
-			for i in range(len(axes)):
-				axes[i] = round(joystick.get_axis(i),0)
+    def OnUpRight(self, *args):
+        self.dc.Move(1, 1)
+            
 
-			(x, y) = (axes[0], -axes[1])
-			dc.Move(x, y)
+    def OnDown(self, *args):
+        self.dc.Move(0, -1)
+            
 
-			background.fill(pygame.Color("white"), (0, 60, 320, 240))
-			background.blit(*AddText("(x, y, speed) = (%d, %d, %d)" % (x, y, dc.GetSpeed()), 10, 60))
+    def OnDownLeft(self, *args):
+        self.dc.Move(-1, -1)
+            
 
-		except KeyboardInterrupt:
-			break
+    def OnDownRight(self, *args):
+        self.dc.Move(1, -1)
+            
 
-		screen.blit(background, (0, 0))
-		pygame.display.flip()
+    def OnLeft(self, *args):
+        self.dc.Move(-1,0)
+            
 
-	dc.Close()
-	pygame.quit()
+    def OnRight(self, *args):
+        self.dc.Move(1, 0)
+            
 
-# Show Time
-Main()
+    def OnStop(self, *args):
+        self.dc.Move(0, 0)
+        
+
+    def _SbSetMessage(self, msg=None):
+        self.StatusBar.pop(self.SbContextId)
+        if(msg!=None):
+            self.StatusBar.push(self.SbContextId, msg)
+
+
+    def _Joystick(self, *args):
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
+        axes = [0]*joystick.get_numaxes()
+        (_x, _y) = (0, 0)
+        while(self.dc!=None):
+            try:
+                pygame.event.pump()
+                for i in range(len(axes)):
+                    axes[i] = round(joystick.get_axis(i),0)
+                (x, y) = (axes[0], -axes[1])
+                if((_x, _y)!=(x, y)):
+                    (_x, _y) = (x, y)
+                    if(x==0 and y==1):
+                        self.OnUp();
+                    elif(x==0 and y==-1):
+                        self.OnDown();
+                    elif(x==-1 and y==1):
+                        self.OnUpLeft();
+                    elif(x==1 and y==1):
+                        self.OnUpRight();
+                    elif(x==-1 and y==-1):
+                        self.OnDownLeft();
+                    elif(x==1 and y==-1):
+                        self.OnDownRight();
+                    elif(x==-1 and y==0):
+                        self.OnLeft();
+                    elif(x==1 and y==0):
+                        self.OnRight();
+                    else:
+                        self.OnStop()
+                time.sleep(0.1)
+            except:
+                break
+
+
+if __name__=="__main__":
+    app = Main()
+    Gtk.main()
